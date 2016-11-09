5d19f418f3991c88e3c7abcb135582679e2e3700
Adam Shumann <ashumann13@gmail.com>
2016-11-09 11:57:49 -0600

    Moved ENVs to entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,7 @@
 FROM node:6
 VOLUME /screeps
-ENV AUTOUPDATE=1 \
-    DB_PATH=/screeps/db.json \
-    ASSET_DIR=/screeps/assets \
-    MOD_DIR=/screeps/mods \
-    GAME_PORT=21025 \
-    GAME_HOST=0.0.0.0 \
-    CLI_PORT=21026 \
-    STORAGE_PORT=21027 \
-    STORAGE_HOST=localhost
-
 WORKDIR /app
 RUN npm install screeps
-EXPOSE 21025 21026
 ADD entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/bin/bash","/entrypoint.sh"]
 CMD ["screeps","start"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,22 @@ term_handler() {
   exit 143; # 128 + 15 -- SIGTERM
 }
 
+
+export AUTOUPDATE=${AUTOUPDATE:-1}
+export DB_PATH=${DB_PATH:-/screeps/db.json}
+export ASSET_DIR=${ASSET_DIR:-/screeps/assets}
+export MOD_DIR=${MOD_DIR:-/screeps/mods}
+export GAME_PORT=${GAME_PORT:-21025}
+export GAME_HOST=${GAME_HOST:-0.0.0.0}
+export CLI_PORT=${CLI_PORT:-21026}
+export STORAGE_PORT=${STORAGE_PORT:-21027}
+export STORAGE_HOST=${STORAGE_HOST:-localhost}
+
 trap term_handler SIGTERM
 
 [[ "$AUTOUPDATE" == "1" ]] && npm update screeps
 
 [[ "$2" == "start" ]] && [ ! -f /screeps/.screepsrc ] && echo "No .screepsrc found, please run 'screeps init' in your data directory" && exit 1
-
+echo Launching $@
+#env
 /app/node_modules/.bin/$@
