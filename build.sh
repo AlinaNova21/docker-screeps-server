86438dfd88e8a40ccfa582c1acd780fc04d6e308
Adam Shumann <ashumann13@gmail.com>
2018-02-07 13:22:07 -0600

    Regex Fail

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 version=$(curl https://raw.githubusercontent.com/screeps/screeps/master/package.json | jq -r .version)
 git clone git@github.com:ags131/docker-screeps-server
 pushd docker-screeps-server
-sed -i 's/^ENV SCREEPS_VERSION.\*$/ENV SCREEPS_VERSION '$version'/i' Dockerfile
+sed -i 's/^ENV SCREEPS_VERSION.*$/ENV SCREEPS_VERSION '$version'/i' Dockerfile
 git commit -a -m "Auto Update to screeps "$version
 git push origin master --tags
+bash
 popd
 rm -rf docker-screeps-server
